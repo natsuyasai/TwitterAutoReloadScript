38cca6b4c0f3042c8bc9888548eb6c0b841fc802
natsuyasai <yasai038tomato@gmail.com>
2023-07-05 21:10:17 +0900

    Update userScript.js

--- userScript.js
+++ userScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         twitter自動読み込み
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @description
 // @author       natsuyasai
 // @match        https://twitter.com/home
@@ -9,10 +9,66 @@
 // @grant        none
 // ==/UserScript==
 
+const BUTTON_ELEMENT_ROOT_ID = "userscript-button-container";
+const BUTTON_ID = "userscript-auto-reload-button";
+const SELECLTED_ELEMENT_ROOT_ID = "userscript-selected-container";
+const SELECTED_LIST_ID = "userscript-interval-setting";
+
+/**
+ * スタイル適用
+ */
+function addStyle() {
+  const css = `
+#${BUTTON_ID} {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 44px;
+  height: 20px;
+  margin: 0.3em;
+  overflow: hidden;
+  border-radius: 4px;
+  box-shadow: 0 2px 10px rgba(0,0,0,0.5);
+  border: none; 
+  text-align: center;
+  background-color: #03A9F4;
+  color: white;
+}
+#${BUTTON_ID} :hover {
+  box-shadow: 1px 6px 15px rgba(0,0,0,0.5);
+}
+ 
+#${BUTTON_ID}:focus {
+  outline: none;
+}
+
+#${SELECTED_LIST_ID} {
+  position: absolute;
+  top: 0;
+  left: 54px;
+  height: 20px;
+  margin: 0.3em;
+  overflow: hidden;
+  border-radius: 4px;
+  box-shadow: 0 2px 10px rgba(0,0,0,0.5);
+  border: none; 
+  text-align: center;
+  background-color: #03A9F4;
+  color: white;
+}
+`;
+const styleElement = document.createElement("style");
+styleElement.innerHTML = css;
+document.head.append(styleElement);
+}
+
+/**
+ * 切り替えボタン追加
+ */
 function addSwithButton() {
   const buttonArea = document.createElement("div");
-  buttonArea.innerHTML = '<button id="userscript-auto-reload-button" type="button" style="position: absolute; top: 0; left: 0">OFF</button>';
-  buttonArea.setAttribute("id", "userscript-button-container");
+  buttonArea.innerHTML = `<button id="${BUTTON_ID}" type="button">OFF</button>`
+  buttonArea.setAttribute("id", BUTTON_ELEMENT_ROOT_ID);
   const mainElement = document.getElementsByTagName("main");
   if (mainElement.length > 0) {
     mainElement[0].appendChild(buttonArea);
@@ -20,7 +76,7 @@ function addSwithButton() {
     document.body.appendChild(buttonArea);
   }
 
-  const button = document.getElementById("userscript-auto-reload-button")
+  const button = document.getElementById(BUTTON_ID);
   button.addEventListener("click", () => {
     isStart = !isStart;
     button.textContent = isStart ? "OFF" : "ON";
@@ -28,11 +84,53 @@ function addSwithButton() {
   }, false);
 }
 
+/**
+ * 読み込み周期設定追加
+ */
 function addIntervalSetting() {
+  const IntervalSecond = Object.freeze({
+    0: 5 ,
+    1: 10,
+    2: 15,
+    3: 30,
+    4: 45,
+    5: 60,
+    6: 120,
+    7: 180,
+  });  
+  const selectedListArea = document.createElement("div");
+  selectedListArea.innerHTML = `
+  <select  id="${SELECTED_LIST_ID}" name="setting" size="1">
+      <option value="0">5秒</option>
+      <option value="1">10秒</option>
+      <option value="2">15秒</option>
+      <option value="3" selected> 30秒</option>
+      <option value="4"> 45秒</option>
+      <option value="5"> 1分</option>
+      <option value="6"> 2分</option>
+      <option value="7"> 3分</option>
+  </select>`
+  selectedListArea.setAttribute("id", SELECLTED_ELEMENT_ROOT_ID);
+  const mainElement = document.getElementsByTagName("main");
+  if (mainElement.length > 0) {
+    mainElement[0].appendChild(selectedListArea);
+  } else {
+    document.body.appendChild(selectedListArea);
+  }
 
+  const listElement = document.getElementById(SELECTED_LIST_ID);
+  listElement.addEventListener("change", (event) => {
+    const value = parseInt(event.currentTarget.value, 10);
+    const intervalSecond = IntervalSecond[value];
+    restartInterval(intervalSecond);
+  }, false);
 }
 
+/**
+ * メイン処理
+ */
 function reSelectTab() {
+  // aタグの中からタグ要素かつ現在アクティブになっている要素を取得し、クリックイベントを発火させる
   const tab = document.getElementsByTagName("a");
   for (let i = 0; i < tab.length; i++) {
     const elem = tab[i];
@@ -47,14 +145,24 @@ function reSelectTab() {
     elem.click();
   }
 }
+
+function restartInterval(intervalSecond) {
+  if (timerId > 0){
+    clearInterval(timerId);
+  }
+  timerId = setInterval(() => {
+    if (isStart) {
+      reSelectTab();
+    }
+  }, 1000 * intervalSecond);
+}
+
 let isStart = true;
 let timerId = -1;
 (function () {
   'use strict';
   addSwithButton();
-  timerId = setInterval(() => {
-    if (isStart) {
-      reSelectTab();
-    }
-  }, 1000 * 30);
+  addIntervalSetting();
+  addStyle();
+  restartInterval(30);
 })();
