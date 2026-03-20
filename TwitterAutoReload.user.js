152c202682920389909806c6d8d7e94c2d2191d2
natsuyasai <4531735+natsuyasai@users.noreply.github.com>
2026-03-20 22:26:33 +0900

    バージョン情報更新

--- TwitterAutoReload.user.js
+++ TwitterAutoReload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter autoload
 // @namespace    https://github.com/natsuyasai/TwitterAutoReloadScript
-// @version      1.5.0
+// @version      1.5.1
 // @description  Automatically retrieve the latest Tweet(X's).
 // @author       natsuyasai
 // @match        https://x.com
